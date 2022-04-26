b1b8c0f815a9947ee6acc6f2b088f367b113eb53
EmmaVLoz <99678919+EmmaVLoz@users.noreply.github.com>
2022-04-25 20:40:30 -0700

    Showing the current temperature

---
+++ src/app.js
@@ -0,0 +1,17 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityELement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityELement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "bae65ac0bad15d50eb38f1156cab5544";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`;
+
+axios.get(apiUrl).then(displayTemperature);
